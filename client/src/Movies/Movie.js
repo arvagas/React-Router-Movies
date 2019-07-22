f09854a546bfae98142dbef204e348492b88290e
Arvin Agas <arvagas@gmail.com>
2019-07-22 18:50:38 -0400

    refactored code to utilize MovieCard.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard'
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
@@ -13,7 +14,6 @@ const Movie = (props) => {
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
-          console.log('is this working', response)
           setMovie(response.data);
         })
         .catch(error => {
@@ -35,22 +35,7 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard title={title} director={director} metascore={metascore} stars={stars}/>
       <div className="save-button">Save</div>
     </div>
   );
